7f07e5ca2a3c8eeb1235ba3b8a52f9dbdab1baee
g-brodiei <chunjuichen@gmail.com>
2019-07-22 21:10:32 +1000

    add Menu component

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,14 +1,14 @@
 <template>
-  <b-navbar type="light" toggleable="lg" class="fixed-top">
+  <b-navbar id="navbar" type="light" toggleable="lg">
     <b-navbar-brand href="/">
       CJ Chen
     </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse" class=""></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item>About</b-nav-item>
+        <b-nav-item href="about">About</b-nav-item>
         <b-nav-item>Service</b-nav-item>
         <b-nav-item>Portfolio</b-nav-item>
         <b-nav-item>Contact</b-nav-item>
@@ -16,7 +16,40 @@
     </b-collapse>
   </b-navbar>
 </template>
-<style>
+<script>
+export default {
+  methods: {
+    handleScroll () {
+      // any code to handle scroll event
+      let nav = document.getElementById('navbar');
+      if (window.scrollY !== 0) {
+        nav.classList.add('fixed-top');
+        nav.classList.remove('sticky-top');
+      } else {
+        nav.classList.add('sticky-top');
+        nav.classList.remove('fixed-top');
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar-light .navbar-toggler {
+   border-color: transparent;
+ }
+
+.navbar-toggler:focus {
+   outline-color: transparent;
+  .navbar-toggler-icon {
+    box-shadow: 1px 2px 1.5px #575757;
+    border-radius: 5px;
+  }
+ }
+
 
 </style>
 
