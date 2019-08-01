c21d2674ffcfad40b6a18c85cc4c69cb865d9549
g-brodiei <chunjuichen@gmail.com>
2019-08-02 00:13:38 +1000

    restyle index.vue for larger device, add animation

--- pages/index.vue
+++ pages/index.vue
@@ -2,44 +2,59 @@
   <div>
     <section id="intro" class="container-fluid" aria-label="introduction">
       <div class="row full-screen-height">
-        <div class="col message-zh">
-          <div class="head-img"><img src="/images/kingfisher.jpg" alt=""></div>
+        <div class="col-12 col-md-8 offset-md-2 message-zh">
+          <div class="head-img ml-3"><img src="/images/kingfisher.jpg" alt=""></div>
           <div class="zh-name" aria-labelledby="ZH-name">
-            <p>我叫</p>
-            <p>陳</p>
-            <p>君</p>
-            <p>瑞</p>
-            <p class="zh-des pt-2" aria-labelledby="ZH-description">一名網頁工程師</p>
+            <p class="zh-punch">我叫陳君瑞</p>
+            <p class="zh-des" aria-labelledby="ZH-description">一名網頁工程師</p>
           </div>
         </div>
-        <div class="col message-en">
+        <div class="col-12 col-md-4 offset-md-2 message-en">
           <h1 class="ml-sm-5 ml-3 title" aria-labelledby="EN-name">
-            <span>Hi,</span>
-            <br class="d-block d-sm-none">
-            <span>I'm Michael Chen</span>
+            I'm Chun-Jui Chen
           </h1>
           <p class="ml-sm-5 ml-3 subtitle" aria-labelledby="EN-description">A Web developer</p>
         </div>
-        <DownArrow />
-      </div>
-    </section>
-    <section id="about" class="container-fluid" aria-label="about">
-      <h2 class="text-center">
-        <nuxt-link to="about">About Me</nuxt-link>
-      </h2>
-      <div class="row">
-        <div class="col">
-        </div>
+      <NavButton />
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import DownArrow from '~/components/DownArrowIndicator'
+import NavButton from '~/components/NavButton'
+
 export default {
   components: {
-    DownArrow
+    NavButton
+  },
+  mounted() {
+    this.$anime
+      .timeline()
+        .add({
+          targets: ".head-img",
+          scale: [0.1,1],
+          opacity: [0,1],
+          easing: "easeOutSine",
+          rotate: 720,
+          duration: 1000,
+          delay: 400
+        })
+        .add({
+          targets: ".zh-name",
+          opacity: [0,1],
+          translateY: 40,
+          easing: "easeOutSine",
+          duration: 800,
+          delay: 200
+        })
+        .add({
+          targets: ".message-en",
+          opacity: [0,1],
+          translateY: -30,
+          easing: "easeOutSine",
+          druation: 800,
+        })
   }
 }
 </script>
@@ -47,50 +62,29 @@ export default {
 <style lang="scss">
 // Full screen settings
 .full-screen-height {
-  height: calc(100vh);
+  height: calc(100vh - 96px);
   position: relative;
 }
 
 // Introduction message
 #intro {
+  
   .message-en {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      position: absolute;
-      bottom: 20%;
-      padding-left:5%;
-    
+    margin-top: 100px;
+    opacity: 0;
     & p {
-      font-family: 'Merienda One', cursive;
-    }
-    
-    .title {
-      :first-child {
-        animation-delay: 200ms;
-        animation-duration: 2000ms;
-      }
-      :last-child {
-        animation-delay: 300ms;
-        animation-duration: 2000ms;
-      }
+      font-family: 'Roboto';
     }
 
     .subtitle {
-      animation-delay: 400ms;
-      animation-duration: 2000ms;
       font-size: 1.2rem;
     }
   }
 
   .message-zh {
-      font-family: "Noto Sans TC";
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      position: absolute;
-      top: 16%;
-      padding-right: 25%;
+    font-family: "Noto Sans TC";
+    margin-top: 50px;
+
 
     p {
       font-family: "Noto Sans TC";
@@ -100,53 +94,42 @@ export default {
     }
 
     .zh-name {
-      :nth-child(1){
-        animation-delay: 200ms;
-        animation-duration: 2000ms;
-      }
-      :nth-child(2){
-        animation-delay: 300ms;
-        animation-duration: 2000ms;
-      }
-      :nth-child(3){
-        animation-delay: 400ms;
-        animation-duration: 2000ms;
-      }
+      opacity: 0;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      // margin-right: -30px;
     }
     .zh-des {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      position: absolute;
-      right: 7%;
-      top: 0%;
       font-size: 1.2rem;
-      animation-delay: 200ms;
-      animation-duration: 1500ms;
-      margin-bottom: -50px;
     }
-  }
-  .head-img {
-    border-radius: 50%;
-    border: 5px #269e93 solid;
-    display: block;
-    position: absolute;
-    overflow: hidden;
-    width: 150px;
-    height: 150px;
-    left: 7%;
-    top: -20%;
-    img {
+
+    .head-img {
+      display: block;
+      opacity: 0;
+      border-radius: 50%;
+      border: 5px rgb(38, 158, 147) solid;
       position: absolute;
-      height: 100%;
-      margin-left: -41px;
+      overflow: hidden;
+      width: 150px;
+      height: 150px;
+      left: 5%;
+      img {
+        position: absolute;
+        height: 100%;
+        margin-left: -41px;
+      }
     }
   }
 }
 
+
+
 @media screen and (min-width:768px) {
   #intro { 
     .message-en {
+
+
       h1 {
         font-size: 2.5rem;
       }
@@ -157,13 +140,17 @@ export default {
     }
 
     .message-zh {
-      padding-right: 18%;
       p {
         font-size: 2.5rem;
       }
       
       .zh-name .zh-des {
         font-size: 1.6rem;
+      }
+
+      .head-img {
+        width: 200px;
+        height: 200px;
       }
     }
   }
@@ -172,7 +159,7 @@ export default {
 @media screen and (max-height: 568px) {
   #intro{
     .message-en {
-      bottom: 15%;
+      
     }
   }
 }
